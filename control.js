64659ef37c0fe09e52d960557bb5d5954414dd24
Elkin Tique <empleado02puntofriomael@hotmail.com>
2024-06-20 01:10:48 -0500

    Empieza El Proyecto Mael V3

---
+++ control.js
@@ -0,0 +1,16 @@
+// este codigonos sirve para tener varios selcionadores de sedes.
+window.addEventListener("DOMContentLoaded", () => {
+    const el = document.getElementById("sedes");
+    el.addEventListener("click", () => {
+       let seleccionados = document.getElementById('sedes').selectedOptions; 
+        for(let i = 0;  i < seleccionados.length; i++){
+          console.log(seleccionados[i].value);
+          }
+      });
+    });
+  
+   function estaderoElFlowDeMael() {
+  características = Promise
+  
+   };
+  
